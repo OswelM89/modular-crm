797dab673b621ab676d1087771ca0e729e9dd5b3
Oswel Matos <oswelm89@gmail.com>
2025-08-23 12:42:34 -0500

    Eliminar texto duplicado del header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -80,7 +80,6 @@ export function Header({ activeSection, onSectionChange }: HeaderProps) {
                 }}
               />
               <span className="text-xl font-bold text-white ml-2">CotizaLow.co</span>
-              <span className="text-xl font-bold text-white ml-2">Modular CRM</span>
             </div>
           </div>
 
